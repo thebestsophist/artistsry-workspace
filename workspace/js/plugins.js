457704d0b7576c4d32d2409ffffeeed1e6a9085f
thebestsophist <bernardy@enguin.com>
2012-03-07 15:26:05 -0600

    Updating window height js
    Updating js to track screen height and window height. Changing cookie names to reflect the meaning of the numbers.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- workspace/js/plugins.js
+++ workspace/js/plugins.js
@@ -16,8 +16,10 @@ window.log = function(){
 
 // Sets window properties as cookies for server-side optimizations
 $(window).resize(function () {
-	document.cookie = "screen-height=" + $(window).height() + "; path=/";
-	document.cookie = "screen-width=" + $(window).width() + "; path=/";
+	document.cookie = "window-width=" + $(window).width() + "; path=/";
+	document.cookie = "window-height=" + $(window).height() + "; path=/";
+	document.cookie = "screen-width=" + screen.width + "; path=/";
+	document.cookie = "screen-height" + screen.height + "; path=/";
 });
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
