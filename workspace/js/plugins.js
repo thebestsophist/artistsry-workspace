1909e3662cd9588ca0eb7feccbf15a0b86b329ac
thebestsophist <bernardy@enguin.com>
2012-03-13 12:04:57 -0500

    Fixing dashes
    Because I don't know how to use a shift key.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- workspace/js/plugins.js
+++ workspace/js/plugins.js
@@ -16,13 +16,13 @@ window.log = function(){
 
 // Sets window properties as cookies for server-side optimizations
 $(window).resize(function () {
-var pixel-density = window.devicePixelRatio || 1,
+var pixel_density = window.devicePixelRatio || 1,
 
 	document.cookie = "window-width=" + $(window).width() + "; path=/";
 	document.cookie = "window-height=" + $(window).height() + "; path=/";
 	document.cookie = "screen-width=" + screen.width + "; path=/";
 	document.cookie = "screen-height=" + screen.height + "; path=/";
-	document.cookie = "pixel-density=" + pixel-density + "; path=/";
+	document.cookie = "pixel-density=" + pixel_density + "; path=/";
 });
 
 window.addEventListener(orientationEvent, function() {
