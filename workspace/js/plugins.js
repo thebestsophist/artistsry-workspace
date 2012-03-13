56e66799d62f5b28cb60d7a78c39148511e42c15
thebestsophist <bernardy@enguin.com>
2012-03-13 12:52:36 -0500

    This worked.
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- workspace/js/plugins.js
+++ workspace/js/plugins.js
@@ -18,7 +18,8 @@ window.log = function(){
 $(window).resize(function () {
 	document.cookie = "window-width=" + $(window).width() + "; path=/";
 	document.cookie = "window-height=" + $(window).height() + "; path=/";
-
+	document.cookie = "screen-width=" + screen.width + "; path=/";
+	document.cookie = "screen-height" + screen.height + "; path=/";
 });
 /*
 function getDevicePixelRatio() {
