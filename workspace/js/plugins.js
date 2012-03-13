0c195b4f4edbe1d5e3ea1fe143fb70cf6a7e6cdd
thebestsophist <bernardy@enguin.com>
2012-03-13 13:20:19 -0500

    Ready + resize function
    Separating everything out into their own function, makin' it purdy, hopin' it works.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- workspace/js/plugins.js
+++ workspace/js/plugins.js
@@ -14,11 +14,6 @@ window.log = function(){
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
 {console.log();return window.console;}catch(err){return window.console={};}})());
 
-// Sets window properties as cookies for server-side optimizations
-$(window).resize(function () {
-	document.cookie = "window-width=" + $(window).width() + "; path=/";
-	document.cookie = "window-height=" + $(window).height() + "; path=/";
-
 function getDevicePixelRatio() {
 	if(window.devicePixelRatio === undefined) return 1; // No pixel ratio available. Assume 1:1.
 	return window.devicePixelRatio;
@@ -29,15 +24,26 @@ function getWindowOrientation() {
 	return window.orientation;
 }
 
+function getWindowProperties() {
+	document.cookie = "window-width=" + $(window).width() + "; path=/";
+	document.cookie = "window-height=" + $(window).height() + "; path=/";
+	document.cookie = 'screen-orientation=' + getWindowOrientation() + '; path=/';
+}
+
+function getScreenProperties() {
 	document.cookie = "screen-width=" + screen.width + "; path=/";
 	document.cookie = "screen-height=" + screen.height + "; path=/";
 	document.cookie = "pixel-density=" + getDevicePixelRatio() + "; path=/";
 
-	document.cookie = 'screen-orientation=' + getWindowOrientation() + '; path=/';
+}
 
-
+// Sets window properties as cookies for server-side optimizations
+$(document).ready(function(){
+	getWindowProperties();
+	getScreenProperties();
 });
-
+$(window).resize(getWindowProperties());
+$(screen).resize(getScreenProperties());
 
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
