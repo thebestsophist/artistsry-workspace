11609112bf8a1f30944202c1c92a62fdc2927f3c
thebestsophist <bernardy@enguin.com>
2012-03-13 12:01:28 -0500

    Adding to screen detection
    Adding orientation + pixel density to screen detection js.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- workspace/js/plugins.js
+++ workspace/js/plugins.js
@@ -16,10 +16,18 @@ window.log = function(){
 
 // Sets window properties as cookies for server-side optimizations
 $(window).resize(function () {
+var pixel-density = window.devicePixelRatio || 1,
+
 	document.cookie = "window-width=" + $(window).width() + "; path=/";
 	document.cookie = "window-height=" + $(window).height() + "; path=/";
 	document.cookie = "screen-width=" + screen.width + "; path=/";
-	document.cookie = "screen-height" + screen.height + "; path=/";
+	document.cookie = "screen-height=" + screen.height + "; path=/";
+	document.cookie = "pixel-density=" + pixel-density + "; path=/";
+});
+
+window.addEventListener(orientationEvent, function() {
+	window-orientation = window.orientation || 0,
+	document.cookie = 'screen-orientation=' + window-orientation + '; path=/';
 });
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
